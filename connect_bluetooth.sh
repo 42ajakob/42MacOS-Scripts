2d7fce31fff34dc959e4804b6850a3e2e6e3284e
ajakob <ajakob@student.42heilbronn.de>
2024-06-15 19:43:35 +0200

    ADD Scripts

---
+++ connect_bluetooth.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Path to blueutils and MacAddress of devices
+blueutil_path="~/.brew/bin/blueutil"
+devices_mac=("XX-XX-XX-XX-XX-XX" "XX-XX-XX-XX-XX-XX" "XX-XX-XX-XX-XX-XX")
+
+# Function to connect devices
+connect_bluetooth() {
+    local device=$1
+	$blueutil_path --connect $device || true
+	$blueutil_path --pair $device || true
+}
+
+# Loop through each device and connect
+for device in "${devices_mac[@]}"; do
+    connect_bluetooth $device
+done
---
+++ disconnect_bluetooth.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Path to blueutils and MacAddress of devices
+blueutil_path="~/.brew/bin/blueutil"
+devices_mac=("XX-XX-XX-XX-XX-XX" "XX-XX-XX-XX-XX-XX" "XX-XX-XX-XX-XX-XX")
+
+# Function to disconnect devices
+disconnect_bluetooth() {
+    local device=$1
+    $blueutil_path --disconnect $device || true
+}
+
+# Loop through each device and disconnect
+for device in "${devices_mac[@]}"; do
+    disconnect_bluetooth $device
+done
